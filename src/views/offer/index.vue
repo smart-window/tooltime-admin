854b84835eb32f46dde5f01d1536b406afca9a57
smart-window <smart.free.dev@gmail.com>
2021-07-09 15:42:47 +0430

    show alert wehn finish the stipe

--- src/views/offer/index.vue
+++ src/views/offer/index.vue
@@ -20,12 +20,31 @@
 import KitList21 from '@/components/kit/widgets/Lists/21'
 import KitList21v1 from '@/components/kit/widgets/Lists/21v1'
 import KitList21v2 from '@/components/kit/widgets/Lists/21v2'
+import * as API from '@/services/api'
+import { message } from 'ant-design-vue'
 
 export default {
   components: {
     KitList21,
     KitList21v1,
     KitList21v2,
+  },
+
+  mounted() {
+    if (this.$route.query.session_id) {
+      this.handleSubmit()
+    }
+  },
+  methods: {
+    async handleSubmit() {
+      try {
+        const res = await API.checkoutSession(this.$route.query.session_id)
+        console.log(res)
+        alert('Congratuations! You phurchased a plan!')
+      } catch (e) {
+        message.error(e.message)
+      }
+    },
   },
 }
 </script>
